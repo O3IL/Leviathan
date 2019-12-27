12e29c5f3417cb5762b1d4d87aef211270246b0c
François-Joseph du Fou <francoisjoseph.du.fou@gmail.com>
2019-12-27 13:09:55 +0100

    Fixes 1.11
    Fixes:
      Zoom
      Find and Replace
      Overlay Tip

--- Leviathan.Services/Leviathan.Mvvm/ViewModels/DocumentViewModel.cs
+++ Leviathan.Services/Leviathan.Mvvm/ViewModels/DocumentViewModel.cs
@@ -281,7 +281,7 @@ namespace Leviathan.Mvvm.ViewModels
             }
         }
 
-        public async Task AddTab()
+        public void AddTab()
         {
             // Add tab
             if (SelectedText.Length > 0)
@@ -290,12 +290,12 @@ namespace Leviathan.Mvvm.ViewModels
                 var text = '\t' + SelectedText;
                 text = text.Replace("\r", "\r\t").TrimEnd('\t');
                 SelectedText = text;
-                await SelectText(currentPosition.start + 1, currentPosition.length);
+                SelectText(currentPosition.start + 1, currentPosition.length, true);
             }
             else
             {
                 SelectedText = "\t";
-                await SelectText(CurrentPosition.start + 1, 0);
+                SelectText(CurrentPosition.start + 1, 0, true);
             }
         }
 
@@ -364,21 +364,18 @@ namespace Leviathan.Mvvm.ViewModels
         public (int start, int length) CurrentPosition
         {
             get => GetPosition?.Invoke() ?? (0, 0);
-            set => SetSelection?.Invoke(value.start, value.length);
+            set => SetSelection?.Invoke(value.start, value.length, false);
         }
 
         public event Func<(int start, int length)> GetPosition;
         public event Action<string> SetSelectedText;
 
-        public async Task SelectText(int start, int length)
+        public void SelectText(int start, int length, bool reindex = true)
         {
-            if (SetSelection != null)
-            {
-                await SetSelection?.Invoke(start, length);
-            }
+            SetSelection?.Invoke(start, length, reindex);
         }
 
-        public event Func<int, int, Task> SetSelection;
+        public event Action<int, int, bool> SetSelection;
 
         private void CalculateHash(string text)
         {
